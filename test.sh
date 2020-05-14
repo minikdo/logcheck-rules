534fb8cf17b6ce06c81ec3b2432cb4292b766337
Dominik Szmek <do@minik.pl>
2020-05-14 18:05:12 +0200

    fix checking regexps: git -f was parsing commented lines

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ failflag=0
 basepath=$(git rev-parse --show-toplevel)
 cd $basepath/ignore.d.server
 for i in `ls`; do
-        egrep -f $i /dev/null #2>/dev/null
+        cat $i | grep -v "^#" | egrep -f - /dev/null #2>/dev/null
         if [ $? -ne 1 ]; then
                 echo "error in server/$i"
                 failflag=1
@@ -20,12 +20,12 @@ for i in `ls`; do
 done
 
 cd $basepath/ignore.d.workstation
-        for i in `ls`; do
-                egrep -f $i /dev/null #2>/dev/null
-                if [ $? -ne 1 ]; then
-                        echo "error in workstation/$i"
-                        failflag=1
-        fi
+for i in `ls`; do
+    cat $i | grep -v "^#" | egrep -f - /dev/null #2>/dev/null
+    if [ $? -ne 1 ]; then
+        echo "error in workstation/$i"
+        failflag=1
+    fi
 done
 
 exit $failflag
